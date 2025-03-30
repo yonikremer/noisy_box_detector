5b5e8832026e7e5e97edc6a9fc1a9d47579c6b95
yonikremer <yoni.kremer@gmail.com>
2025-03-30 22:42:55 +0300

    bugfix: used to generate out of bounds triangle

--- rectangle.py
+++ rectangle.py
@@ -91,10 +91,10 @@ class Rectangle(BaseModel):
 
     @classmethod
     def random(cls):
-        x = random.randint(0, IMAGE_WIDTH)
-        y = random.randint(0, IMAGE_HEIGHT)
+        x = random.randint(0, IMAGE_WIDTH - MIN_RECTANGLE_WIDTH)
+        y = random.randint(0, IMAGE_HEIGHT - MIN_RECTANGLE_HEIGHT)
         max_width = IMAGE_WIDTH - x
         max_height = min(IMAGE_HEIGHT - y, MAX_RECTANGLE_HEIGHT)
-        rect_width = random.randint(MIN_RECTANGLE_WIDTH, max_width)
-        rect_height = random.randint(MIN_RECTANGLE_HEIGHT, max_height)
+        rect_width = random.randint(MIN_RECTANGLE_WIDTH, max_width - 1)
+        rect_height = random.randint(MIN_RECTANGLE_HEIGHT, max_height - 1)
         return Rectangle(x=x, y=y, height=rect_height, width=rect_width)
